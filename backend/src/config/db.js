78f4d68d9235b9eed0674ec311ea99a0fffeb76b
Swayam Kelkar <67506271+Ecstaticvanilla@users.noreply.github.com>
2026-06-25 14:03:16 +0530

    First Step Journey Before Destination

---
+++ backend/src/config/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+try {
+    await mongoose.connect(MON)
+} catch (error) {
+    
+}
---
+++ backend/src/server.js
@@ -0,0 +1,3 @@
+import express from "express";
+import connectDB from "./config/db.js"
+
